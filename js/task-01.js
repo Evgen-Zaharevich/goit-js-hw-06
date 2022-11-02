aedbd973993fc30266a940ce792f30c6a30c9f3e
Evgen Zaharevich <metrovyy@gmail.com>
2022-11-02 13:43:38 +0200

    finish task-01

--- js/task-01.js
+++ js/task-01.js
@@ -20,9 +20,22 @@
 // Elements: 5
 // =================================================================================================================
 
-const categories = document.querySelectorAll(`.item`)
-console.log(`Number of categories: ${categories.length}`);
+const categoriesRef = document.querySelectorAll(`.item`)
+console.log(`Number of categories: ${categoriesRef.length}`);
+console.log(` `);
 
-const descendantsCategories = [...categories.children];
+const categoryAnimalRef = categoriesRef[0].firstElementChild;
+console.log(`Category: ${categoryAnimalRef.textContent}`)
+console.log(`Elements: ${categoryAnimalRef.nextElementSibling.children.length}`)
+console.log(` `);
 
-console.log(descendantsCategories); 
+const categoryProductsRef = categoriesRef[1].firstElementChild;
+console.log(`Category: ${categoryProductsRef.textContent}`)
+console.log(`Elements: ${categoryProductsRef.nextElementSibling.children.length}`)
+console.log(` `);
+
+const categoryTechnologiesRef = categoriesRef[1].firstElementChild;
+console.log(`Category: ${categoryTechnologiesRef.textContent}`)
+console.log(`Elements: ${categoryTechnologiesRef.nextElementSibling.children.length}`)
+console.log(` `);
+
