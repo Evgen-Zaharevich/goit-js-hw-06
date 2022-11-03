c8f330079d3b2ed6a97daee6b144d17b020cd1e8
Evgen Zaharevich <metrovyy@gmail.com>
2022-11-03 04:10:17 +0200

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const refInput = document.querySelector(`#validation-input`);
+const dataLength = refInput.getAttribute(`data-length`)
+
+refInput.addEventListener(`blur`, (event) => {
+    if (event.currentTarget.value.length === Number(dataLength)) {
+        refInput.classList.add(`valid`);
+        refInput.classList.remove(`invalid`);
+    } else {
+        refInput.classList.add(`invalid`);
+        refInput.classList.remove(`valid`);
+    }
+})
