fc35005411639ec7b80c8eb8fea3add8a5e04413
Evgen Zaharevich <metrovyy@gmail.com>
2022-11-03 20:17:14 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refButton = document.querySelector(".change-color");
+const refText = document.querySelector(".color");
+const refBody = document.body;
+
+
+refButton.addEventListener(`click`, changeBackgroundColor)
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBackgroundColor(event) {
+  refBody.style.backgroundColor = getRandomHexColor();
+  refText.textContent = getRandomHexColor();
 }
